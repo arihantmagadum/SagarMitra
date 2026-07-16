bc02769c17e19ad4412ff6eee9f2f37ed0b86132
Arihant Magadum <arihantmagadum18@gmail.com>
2026-07-16 15:13:36 +0530

    Add files via upload

---
+++ .idea/mvn/src/main/test/java/com/sagaramitra/SagaraMitraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HonnaBot.SagaraMitra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SagaraMitraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
